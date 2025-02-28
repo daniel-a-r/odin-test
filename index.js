8d22cf231a8fd48e2628519e02f26cdc0efd1b8b
daniel-a-r <daniel.a-r@outlook.com>
2025-02-28 05:28:10 -0500

    create tests for functions

--- index.js
+++ index.js
@@ -1,3 +1,84 @@
-export const sum = (a, b) => {
-  return a + b;
+export const capitalize = (s) => {
+  return s.at(0).toUpperCase() + s.slice(1);
+};
+
+export const reverseString = (s) => {
+  const chars = s.split('');
+  for (let i = 0; i < Math.floor(chars.length / 2); i++) {
+    const temp = chars.at(i);
+    chars[i] = chars.at(chars.length - i - 1);
+    chars[chars.length - i - 1] = temp;
+  }
+  return chars.join('');
+};
+
+export const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+
+  subtract(a, b) {
+    return a - b;
+  },
+
+  multiply(a, b) {
+    if (a === 0 || b === 0) {
+      return 0;
+    }
+
+    return a * b;
+  },
+
+  divide(a, b) {
+    if (b === 0) {
+      return NaN;
+    } else if (a === 0) {
+      return 0;
+    }
+
+    return a / b;
+  },
+};
+
+export const ceasarCipher = (s, shift) => {
+  if (shift >= 26) {
+    shift = shift % 26;
+  }
+
+  const chars = s.split('');
+
+  chars.forEach((c, i) => {
+    const charCode = c.charCodeAt(0);
+    if (
+      (charCode >= 65 && charCode <= 90) ||
+      (charCode >= 97 && charCode <= 122)
+    ) {
+      const shifted = charCode + shift;
+      if (charCode >= 97 && shifted > 122) {
+        chars[i] = String.fromCharCode(shifted - 123 + 97);
+      } else if (charCode <= 90 && shifted > 90) {
+        chars[i] = String.fromCharCode(shifted - 91 + 65);
+      } else {
+        chars[i] = String.fromCharCode(shifted);
+      }
+    }
+  });
+
+  return chars.join('');
+};
+
+const calcAvg = (arr) => {
+  const initialVal = 0;
+  const sum = arr.reduce((accumlator, currentVal) => {
+    return accumlator + currentVal;
+  }, initialVal);
+  return sum / arr.length;
+};
+
+export const analyzeArray = (arr) => {
+  const average = calcAvg(arr);
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const length = arr.length;
+  return { average, min, max, length };
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,226 @@
 import * as f from './index.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(f.sum(1, 2)).toBe(3);
+describe('Capitalize first character of a string', () => {
+  test('All lowercase', () => {
+    expect(f.capitalize('hello world')).toBe('Hello world');
+  });
+
+  test('First letter lowercase, rest uppercase', () => {
+    expect(f.capitalize('hELLO WORLD')).toBe('HELLO WORLD');
+  });
+
+  test('First letter already uppercase', () => {
+    expect(f.capitalize('World hello')).toBe('World hello');
+  });
+
+  test('Punctuation as the first character', () => {
+    expect(f.capitalize('!now with punctuation')).toBe('!now with punctuation');
+  });
+});
+
+describe('String reverse', () => {
+  test('reverse a correct string', () => {
+    expect(f.reverseString('hello world')).toBe('dlrow olleh');
+  });
+
+  test('reverse a reversed string', () => {
+    expect(f.reverseString('dlrow olleh')).toBe('hello world');
+  });
+
+  test('reverse a palidrome', () => {
+    expect(f.reverseString('Racecar')).toBe('racecaR');
+  });
+});
+
+describe('Calculator add', () => {
+  test('add the same positive numbers', () => {
+    expect(f.calculator.add(1, 1)).toBe(2);
+  });
+
+  test('add two different positive numbers', () => {
+    expect(f.calculator.add(1, 2)).toBe(3);
+  });
+
+  test('add a positive and zero', () => {
+    expect(f.calculator.add(0, 3)).toBe(3);
+  });
+
+  test('add two zeros', () => {
+    expect(f.calculator.add(0, 0)).toBe(0);
+  });
+
+  test('add a positive and negative', () => {
+    expect(f.calculator.add(-1, 1)).toBe(0);
+  });
+
+  test('add two negative numbers', () => {
+    expect(f.calculator.add(-1, -1)).toBe(-2);
+  });
+
+  test('add a negative and zero', () => {
+    expect(f.calculator.add(-1, 0)).toBe(-1);
+  });
+});
+
+describe('Calculator subtract', () => {
+  test('subtract the same positive numbers', () => {
+    expect(f.calculator.subtract(1, 1)).toBe(0);
+  });
+
+  test('subtract two different positive numbers', () => {
+    expect(f.calculator.subtract(1, 2)).toBe(-1);
+  });
+
+  test('subtract a positive and zero', () => {
+    expect(f.calculator.subtract(0, 3)).toBe(-3);
+  });
+
+  test('subtract two zeros', () => {
+    expect(f.calculator.subtract(0, 0)).toBe(0);
+  });
+
+  test('subtract a positive and negative', () => {
+    expect(f.calculator.subtract(-1, 1)).toBe(-2);
+  });
+
+  test('subtract two negative numbers', () => {
+    expect(f.calculator.subtract(-1, -1)).toBe(0);
+  });
+
+  test('subtract a negative and zero', () => {
+    expect(f.calculator.subtract(-1, 0)).toBe(-1);
+  });
+});
+
+describe('Calculator multiply', () => {
+  test('multiply the same positive numbers', () => {
+    expect(f.calculator.multiply(1, 1)).toBe(1);
+  });
+
+  test('multiply two different positive numbers', () => {
+    expect(f.calculator.multiply(1, 2)).toBe(2);
+  });
+
+  test('multiply a positive and zero', () => {
+    expect(f.calculator.multiply(0, 3)).toBe(0);
+  });
+
+  test('multiply two zeros', () => {
+    expect(f.calculator.multiply(0, 0)).toBe(0);
+  });
+
+  test('multiply a positive and negative', () => {
+    expect(f.calculator.multiply(-1, 1)).toBe(-1);
+  });
+
+  test('multiply two negative numbers', () => {
+    expect(f.calculator.multiply(-1, -1)).toBe(1);
+  });
+
+  test('multiply a negative and zero', () => {
+    expect(f.calculator.multiply(-1, 0)).toBe(0);
+  });
+});
+
+describe('Calculator divide', () => {
+  test('divide the same positive numbers', () => {
+    expect(f.calculator.divide(1, 1)).toBe(1);
+  });
+
+  test('divide two different positive numbers', () => {
+    expect(f.calculator.divide(1, 2)).toBe(0.5);
+  });
+
+  test('divide zero by a positive', () => {
+    expect(f.calculator.divide(0, 3)).toBe(0);
+  });
+
+  test('divide two zeros', () => {
+    expect(f.calculator.divide(0, 0)).toBeNaN();
+  });
+
+  test('divide a positive and negative', () => {
+    expect(f.calculator.divide(-1, 1)).toBe(-1);
+  });
+
+  test('divide two negative numbers', () => {
+    expect(f.calculator.divide(-1, -1)).toBe(1);
+  });
+
+  test('divide zero by a negative', () => {
+    expect(f.calculator.divide(0, -1)).toBe(0);
+  });
+});
+
+describe('ceasar cipher', () => {
+  test('start of alphabet, lowercase', () => {
+    expect(f.ceasarCipher('abc', 3)).toBe('def');
+  });
+
+  test('start of alphabet, lowercase looping back to start', () => {
+    expect(f.ceasarCipher('abc', 26)).toBe('abc');
+  });
+
+  test('start of alphabet, lowercase with shift over 26', () => {
+    expect(f.ceasarCipher('abc', 29)).toBe('def');
+  });
+
+  test('start of alphabet, uppercase', () => {
+    expect(f.ceasarCipher('ABC', 3)).toBe('DEF');
+  });
+
+  test('start of alphabet, uppercase looping back to start', () => {
+    expect(f.ceasarCipher('ABC', 26)).toBe('ABC');
+  });
+
+  test('start of alphabet, uppercase with shift over 26', () => {
+    expect(f.ceasarCipher('ABC', 29)).toBe('DEF');
+  });
+
+  test('end of alphabet, lowercase', () => {
+    expect(f.ceasarCipher('xyz', 3)).toBe('abc');
+  });
+
+  test('end of alphabet, uppercase', () => {
+    expect(f.ceasarCipher('XYZ', 3)).toBe('ABC');
+  });
+
+  test('with spaces and punctuation', () => {
+    expect(f.ceasarCipher('Hello, world!', 3)).toBe('Khoor, zruog!');
+  });
+});
+
+describe('analyze array', () => {
+  test('All zeros', () => {
+    const arr = [0, 0, 0, 0, 0];
+    const expected = {
+      average: 0,
+      min: 0,
+      max: 0,
+      length: 5,
+    };
+    expect(f.analyzeArray(arr)).toEqual(expected);
+  });
+
+  test('all ones', () => {
+    const arr = [1, 1, 1, 1, 1];
+    const expected = {
+      average: 1,
+      min: 1,
+      max: 1,
+      length: 5,
+    };
+    expect(f.analyzeArray(arr)).toEqual(expected);
+  });
+
+  test('random positive numbers', () => {
+    const arr = [1, 8, 3, 4, 2, 6];
+    const expected = {
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    };
+    expect(f.analyzeArray(arr)).toEqual(expected);
+  });
 });
